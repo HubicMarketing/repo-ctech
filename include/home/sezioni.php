de2744b2ab50d83859fd7004319349428683cfc2
Hubic Marketing <stefanoorlandi@hubicmarketing.it>
2020-06-16 11:19:03 +0200

    update cta

--- include/home/sezioni.php
+++ include/home/sezioni.php
@@ -2,7 +2,7 @@
   <?php if(is_front_page()) { ?>
   <div class="heading_wrapper">
     <h2 class="animated wow fadeInLeft" data-wow-duration="0.5s" data-wow-delay="0.5s">Scopri le nostre soluzioni di noleggio e vendita</h2>
-    <a class="animated wow fadeInRight" data-wow-duration="0.6s" data-wow-delay="0.6s" href="#form" class="cta"><strong>Contattaci per <br> maggiori informazioni</strong></a>
+    <a class="cta animated wow fadeInRight" data-wow-duration="0.6s" data-wow-delay="0.6s" href="#form"><strong>Contattaci per <br> maggiori informazioni</strong></a>
   </div>
   <?php } ?>
 <div class="brand_container row">
