34ea9dd0daef43ebe2d0192d15aa911020d8422d
Hubic Marketing <stefanoorlandi@hubicmarketing.it>
2020-06-19 16:12:25 +0200

    fix azienda top sliding banner

--- header.php
+++ header.php
@@ -38,7 +38,7 @@
 		?>
 
 		<header id="site-header" class="header-footer-group" role="banner">
-				<?php include( get_stylesheet_directory() . '/include/top_bar.php'); ?>
+				<?php if(!my_wp_is_mobile() && is_page('azienda')) { include( get_stylesheet_directory() . '/include/top_bar.php'); } ?>
 			<div class="header-inner section-inner">
 				<div class="header-titles-wrapper">
 
