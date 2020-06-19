81af5c48ca8b6bebd92150271eea7350f2cd83e6
Hubic Marketing <stefanoorlandi@hubicmarketing.it>
2020-06-19 12:32:59 +0200

    fix product image gallery

--- functions.php
+++ functions.php
@@ -311,12 +311,13 @@ add_action( 'woocommerce_product_thumbnails', 'woocommerce_template_single_meta'
 // 	  yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
 // 	}
 // }
-// add_action( 'woocommerce_after_single_product_summary', 'add_gallery' );
 
+add_action( 'woocommerce_after_single_product_summary', 'add_gallery' );
 function add_gallery() {
 	include( get_stylesheet_directory() . '/include/prodotti/gallery.php');
 	// include( get_stylesheet_directory() . '/include/prodotti/vantaggi.php');
 }
+
 add_action( 'woocommerce_after_single_product_summary', 'add_form_prodotto', 12 );
 function add_form_prodotto() {
 	include( get_stylesheet_directory() . '/include/prodotti/form_prodotto.php');
