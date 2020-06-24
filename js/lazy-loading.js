7b48051167314bc025942cf84260a59bee512b63
Hubic Marketing <stefanoorlandi@hubicmarketing.it>
2020-06-24 11:15:01 +0200

    fix lazy load

--- js/lazy-loading.js
+++ js/lazy-loading.js
@@ -1,16 +1,19 @@
-
-    if ('loading' in HTMLImageElement.prototype) {
-      const images = document.querySelectorAll("img.lazyload");
-      images.forEach(img => {
-        const source = img.getAttribute('src');
-        img.setAttribute('data-src',source);
-        img.removeAttribute('src');
-      });
-    } else {
-      // Dynamically import the LazySizes library
-      let script = document.createElement("script");
-      script.async = true;
-      script.src =
-        "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js";
-      document.body.appendChild(script);
-    }
+document.addEventListener('DOMContentLoaded', function(){
+  if ('loading' in HTMLImageElement.prototype) {
+    const images = document.querySelectorAll("img.lazyload");
+    images.forEach(img => {
+      // img.src = img.dataset.src;
+      const source = img.getAttribute('src');
+      img.setAttribute('data-src',source);
+      img.removeAttribute('src');
+    });
+  } else {
+    // Dynamically import the LazySizes library
+    let script = document.createElement("script");
+    script.async = true;
+    script.src =
+      "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js";
+    document.body.appendChild(script);
+  }
+});
+    
